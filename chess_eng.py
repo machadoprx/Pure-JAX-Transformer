0e90bfe038cde2f74afad9a5ea8ad10c81f76170
Victor Machado <vmachado@pop-os.localdomain>
2022-07-16 14:03:06 -0300

    first

---
+++ chess_eng.py
@@ -0,0 +1,14 @@
+import chess
+import chess.pgn
+import bz2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+
+#print(chess_eng.__file__)
+board = chess.Board()
+
+#board = first_game.board()
+
+print(board)
